8c2ae66e1f7da3911d2409a11ce43c95e8342468
mollyexten <molly.exten@gmail.com>
2021-03-17 16:47:10 -0400

    Attributed background photo with link to Wikimedia Commons

--- src/App.js
+++ src/App.js
@@ -30,6 +30,17 @@ function App() {
           <Station />
         </Route>
       </Switch>
+      <footer>
+        Photo credit: <a href=
+          "https://commons.wikimedia.org/wiki/File:Outbound_train_at_Charles_MGH_station,_May_2006.jpg"
+        >
+          Adam E. Moreira
+        </a>,
+        <a href="http://creativecommons.org/licenses/by-sa/3.0/">
+          CC BY-SA 3.0
+        </a>
+        , via Wikimedia Commons
+      </footer>
     </div>
   );
 }
