e0ed1a17274d1f993ea10dd230563f95f0a11a74
mollyexten <molly.exten@gmail.com>
2021-03-17 17:03:12 -0400

    stylized buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 function Navbar() {
   return (
     <nav>
-      <Link to="/">HOME</Link>
-      <Link to="/about">ABOUT</Link>
-      <Link to="/contribute">CONTRIBUTE</Link>
+      <Link to="/"><button>Home</button></Link>
+      <Link to="/about"><button>About</button></Link>
+      <Link to="/contribute"><button>Share Ideas</button></Link>
     </nav>
   )
 }
