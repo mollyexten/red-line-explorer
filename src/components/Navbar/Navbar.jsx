fe1695f78ac5528f36d83adff5046b25aa94f4cd
mollyexten <molly.exten@gmail.com>
2021-03-19 14:00:10 -0400

    Updated css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,19 +4,28 @@ import "./Navbar.css";
 
 function Navbar() {
   // I referred to this website for creating a bottom navbar on mobile: https://jfelix.info/blog/create-a-mobile-friendly-navigation-with-react
+
+  // Save windowDimension in state variable
   const [windowDimension, setWindowDimension] = useState(null);
+
+  // When page loads, capture the inner width of the window
   useEffect(() => {
     setWindowDimension(window.innerWidth);
   }, []);
 
+  
   useEffect(() => {
+    // This function will reset the window dimension when invoked
     function handleResize() {
       setWindowDimension(window.innerWidth);
     }
+    // When the component mounts, this adds handleResize as an event listener
     window.addEventListener("resize", handleResize);
+    // When the component unmounts, the event listener is removed so that it doesn't interfere with other pages' windows(?)
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  // This variable will be used in the JSX to determine which view to render
   const isMobile = windowDimension <= 640;
 
   return (
