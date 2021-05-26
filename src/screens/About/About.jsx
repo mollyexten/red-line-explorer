25f5103c1e4a88e2d875d9eae2f666a25c3fe401
mollyexten <molly.exten@gmail.com>
2021-05-26 13:39:06 -0400

    styling

--- src/screens/About/About.jsx
+++ src/screens/About/About.jsx
@@ -9,27 +9,28 @@ function About() {
         <h1 className="header-bottom">&nbsp;</h1>
       </header>
       <div>
-        <div className="about-text">
-          <h3>About This Site</h3>
-          <p>
+        <div className="about-container">
+                  <section className="about-div">
+          <h3 className="about-header">About This Site</h3>
+          <p className="about-paragraph">
             Red Line Explorer is a place for users to discover and contribute
             places of interest near MBTA Red Line stations. This website is
             useful for out-of-towners looking for places outside of the regular
             tourist route and locals who want a different perspective on their
             surroundings.
           </p>
-          <p>
+          <p className="about-paragraph">
             This website depends on the contributions of users like you! If you
             know of any interesting spots around a particular Red Line station,
             please share your experiences!
           </p>
-        </div>
+        </section>
         <Link to="/share-ideas">
           <button className="share-ideas">Share Ideas</button>
         </Link>
-        <div className="about-text">
-          <h3>About Me</h3>
-          <p>
+        <section className="about-div">
+          <h3 className="about-header">About Me</h3>
+          <p className="about-paragraph">
             Hi, I’m Molly, and I am the creator of Red Line Explorer! Since
             moving to Boston in 2013, I have spent more time than I care to
             count waiting for and riding on MBTA vehicles. Throughout all my
@@ -38,7 +39,7 @@ function About() {
             normal”, I hope that we give public transportation the support it
             needs. It is an essential part of society.
           </p>
-          <p>
+          <p className="about-paragraph">
             Never grateful when you’re on time, always annoyed when you’re
             twenty minutes away, I dedicate this site to you, Red Line.
           </p>
@@ -53,7 +54,9 @@ function About() {
             </a>
             , via Wikimedia Commons
           </p>
+        </section>
         </div>
+
       </div>
     </div>
   );
